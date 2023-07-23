6cb6c1118e5a705aa544fe9c06393a4227204d3e
Gilberto <Betho6990@gmail.com>
2023-07-23 12:48:28 -0600

    Creating rest controller products

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -1,4 +1,4 @@
-package com.formacionbdi.springboot.app.productos.springbootservicioproductos;
+package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
