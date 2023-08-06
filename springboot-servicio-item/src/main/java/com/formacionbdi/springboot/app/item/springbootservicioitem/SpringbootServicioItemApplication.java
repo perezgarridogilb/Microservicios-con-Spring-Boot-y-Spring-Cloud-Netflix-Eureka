848bde063da811d14b9395336573067c32fbedf6
Gilberto <Betho6990@gmail.com>
2023-08-05 23:33:27 -0600

    Connecting our services as Eureka clients

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/springbootservicioitem/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/springbootservicioitem/SpringbootServicioItemApplication.java
@@ -2,10 +2,14 @@ package com.formacionbdi.springboot.app.item.springbootservicioitem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "servicio-productos")
+@EnableEurekaClient
+/** 
+ * Necesario debido a que necesitamos feign como cliente para conectar a las API rest 
+ * Ribbon ya viene ímplicito en eureka
+*/
 @EnableFeignClients
 
 @SpringBootApplication
--- springboot-servicio-productos/README.md
+++ springboot-servicio-productos/README.md
@@ -7,3 +7,6 @@ spring.application.name=servicio-productos
 server.port=8001
 ```
 ![FireShot Capture 040 - Microservicios con Spring Boot y Spring Cloud Netflix Eureka - Udemy_ - www udemy com](https://github.com/perezgarridogilb/Microservicios-con-Spring-Boot-y-Spring-Cloud-Netflix-Eureka/assets/56992179/4d7f1f16-e63f-451b-99ec-336ea6228df9)
+
+## Creating Eureka registry server
+Estamos configurando a item y productos como clientes de eureka
